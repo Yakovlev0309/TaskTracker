70e855cd3ab2fff6c0d108790dacee893be8451f
Yakovlev_Nikita <yakovlev.universe@gmail.com>
2026-06-26 21:29:05 +0300

    various colors in terminal

--- include/PrintSystem.hpp
+++ include/PrintSystem.hpp
@@ -7,16 +7,30 @@
 
 namespace Print
 {
+namespace Color
+{
+    constexpr const char* Reset  = "\033[0m";
+    constexpr const char* Red    = "\033[31m";
+    constexpr const char* Green  = "\033[32m";
+    constexpr const char* Yellow = "\033[33m";
+}
+
     template <typename T>
     void Info(T msg)
     {
-        std::cout << "[Info]: " << msg << std::endl;
+        std::cout << Color::Green << "[Info]: " << msg << Color::Reset << std::endl;
+    }
+
+    template <typename T>
+    void Warning(T msg)
+    {
+        std::cerr << Color::Yellow << "[Warning]: " << msg << Color::Reset << std::endl;
     }
 
     template <typename T>
     void Error(T msg)
     {
-        std::cerr << "[Error]: " << msg << std::endl;
+        std::cerr << Color::Red << "[Error]: " << msg << Color::Reset << std::endl;
     }
 
     template<std::ranges::input_range Range>
@@ -24,7 +38,9 @@ namespace Print
     {
         for (const auto& [id, task] : tasks)
         {
-            std::cout << std::format("{}: \"{}\" [{}]", id, task.title, Task::StatusToString(task.status)) << std::endl;
+            std::cout << Color::Green << 
+                std::format("{}: \"{}\" [{}]", id, task.title, Task::StatusToString(task.status)) 
+                << Color::Reset << std::endl;
         }
     }
 }
